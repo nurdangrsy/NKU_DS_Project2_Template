2269ad9b4f890688d69fecaa42340ea9e32f4d3b
Güvenç Usanmaz <gusanmaz@gmail.com>
2022-04-05 09:55:31 +0300

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,14 @@
+public class Node {
+    public int value;
+    public Node next;
+
+    public Node(int v){
+        value = v;
+        next  = null;
+    }
+
+    public Node(){
+        
+    }
+
+}
